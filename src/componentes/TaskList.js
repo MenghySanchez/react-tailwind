ea026bb1f1f1ebdcddb4bd79c68b9572982adfbf
Menghy Sánchez <47823011+MenghySanchez@users.noreply.github.com>
2022-08-06 16:55:35 -0500

    metodo para buscar las tareas

--- src/componentes/TaskList.js
+++ src/componentes/TaskList.js
@@ -1,18 +1,25 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
+import { useSelector , useDispatch} from "react-redux";
+import { deleteTask } from "../tasks/taskSlice";
 
 export default function TaskList() {
-
   const tasks = useSelector((state) => state.tasks);
-  console.log(tasks)
+  const dispatch = useDispatch()
+
+
+  console.log(tasks);
+  const handelDelete = (id) => {
+    dispatch(deleteTask(id))
+  };
 
   return (
     <div>
-      {tasks.map(task => (
-        <div key =  {task.id}>
+      {tasks.map((task) => (
+        <div key={task.id}>
           <h2>{task.title}</h2>
           <p> {task.descripcion} </p>
+          <button onClick={() => handelDelete(task.id)}>Eliminar</button>
         </div>
       ))}
     </div>
--- src/tasks/taskSlice.js
+++ src/tasks/taskSlice.js
@@ -13,26 +13,29 @@ const initialState = [
     descripcion: "Descripcion de la tarea 2",
     completed: false,
   },
-]
+];
 
 export const taskSlice = createSlice({
   name: "tasks",
   initialState,
   reducers: {
     addTask: (state, action) => {
-      state.push(action.payload)
-/**
- *  [...state, action.payload]
- * la linea anterior es la forma como tradicionalmente se agrega 
- * un elemento con react, sin embargo para comprension mas simple desde js
- * usamo la funcion push , asi es mas sencillo entender que estamos
- * cargado un bloque de info
- */      
+      // metodo para agregar la tarea
+      state.push(action.payload);
+      /**
+       *  [...state, action.payload]
+       * la linea anterior es la forma como tradicionalmente se agrega
+       * un elemento con react, sin embargo para comprension mas simple desde js
+       * usamo la funcion push , asi es mas sencillo entender que estamos
+       * cargado un bloque de info
+       */
     },
-    
+    deleteTask: (state, action) => {
+      const taskFound = state.find((task) => task.id === action.payload);
+      console.log(taskFound);
+    },
   },
 });
 
-
-export const {addTask} = taskSlice.actions
+export const { addTask, deleteTask } = taskSlice.actions;
 export default taskSlice.reducer;
