30ba750c579db727662ef9b692e8f573224c9ea6
Menghy Sánchez <47823011+MenghySanchez@users.noreply.github.com>
2022-08-09 05:19:03 -0500

    add stile to tasklist

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
 /* Setting the background color to slate-900 and the text color to slate-300. */
-    <div className="bg-slate-900 h-screen text-slate-300">
-      <h1> Kanban Table</h1>
+    <div className="bg-slate-900 
+                    min-h-screen
+                    scroll-m-1 
+                  text-slate-300
+                  ">
+
+      <div className="justify-center text-center">
+      <h1 className="font-bold text-2xl py-2"> Notes Table</h1>
+      </div>
+      
       <div className="flex items-center justify-center h-full">
       <BrowserRouter>
         <Routes>
--- src/componentes/TaskList.js
+++ src/componentes/TaskList.js
@@ -20,26 +20,73 @@ export default function TaskList() {
   };
 
   return (
-    <div>
-      <header>
-        <h1>Actividades ({tasks.length})</h1>
-        <Link to="/create-task">Crear Tarea </Link>
+    <div className="w-4/6">
+      <header className="flex justify-between items-center py-3">
+        <h1 className="font-bold" >Actividades ({tasks.length})</h1>
+        <Link to="/create-task" 
+          className="
+            bg-indigo-600
+            hover:bg-indigo-700 
+            focus:ring-indigo-500 
+            focus:ring-offset-indigo-200 
+            text-white 
+              transition ease-in duration-200 text-center 
+              text-xs 
+              font-semibold 
+              shadow-md 
+              focus:outline-none 
+              focus:ring-2 
+              focus:ring-offset-2 
+              py-1 px-2 
+              rounded-lg"
+        >
+          Crear Tarea{" "}
+        </Link>
       </header>
 
       <div>
-        {tasks.map((task) => (
-          <div key={task.id}>
-            <header>
-              <h3>{task.title}</h3>
-              <div>
-                <Link to={`/edit-task/${task.id}`}>Edit</Link>
-                <button onClick={() => handleDelete(task.id)}>Eliminar</button>
-              </div>
-            </header>
-            <p>{task.descripcion}</p>
-            <p>{task.id}</p>
-          </div>
-        ))}
+        <div className="grid grid-cols-2 gap-4">
+          {tasks.map((task) => (
+            <div
+              className="text-center bg-neutral-800 p-4 rounded-lg"
+              key={task.id}
+            >
+              <header className="flex justify-between">
+                <h3 className="font-semibold">{task.title}</h3>
+                <div className="flex gap-2.5">
+                  <button
+                    className="py-1 px-2   
+                  bg-red-600 
+                  hover:bg-red-700 
+                  focus:ring-red-500 
+                  focus:ring-offset-indigo-200 
+                  text-white 
+                    w-full 
+                    transition ease-in duration-200 text-center 
+                    text-xs font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                    onClick={() => handleDelete(task.id)}
+                  >
+                    Eliminar
+                  </button>
+                  <Link
+                    className="py-1 px-2 w-full
+                  bg-slate-600
+                  hover:bg-slate-700
+                  focus:ring-slate-500 
+                  focus:ring-offset-slate-200 
+                  text-white
+                    transition ease-in duration-200 text-center 
+                    font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg text-xs"
+                    to={`/edit-task/${task.id}`}
+                  >
+                    Editar
+                  </Link>
+                </div>
+              </header>
+              <p className="py-3 ">{task.descripcion}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
